70a6e604c10399e9cc33a42b6afa5c6355573398
qJay44 <m70155302@yandex.ru>
2023-09-19 17:41:03 +0300

    update

--- script.meta.js
+++ script.meta.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Scroll to last played album
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  scroll to last played album by saving scroll position
 // @author       qJay44
-// @homepage     https://github.com/Sak32009/GetDLCInfoFromSteamDB/
-// @homepageURL  https://github.com/Sak32009/GetDLCInfoFromSteamDB/
-// @downloadURL  https://raw.githack.com/qJay44/scroll-to-album/user.js
-// @updateURL    https://raw.githack.com/qJay44/scroll-to-album/meta.js
+// @source       github:qJay44/scroll-to-album
+// @homepage     https://github.com/qJay44/scroll-to-album/
+// @homepageURL  https://github.com/qJay44/scroll-to-album/
+// @downloadURL  https://raw.githack.com/qJay44/main/scroll-to-album/script.user.js
+// @updateURL    https://raw.githack.com/qJay44/main/scroll-to-album/script.meta.js
 // @match        https://vk.com/audios-60027733?section=playlists
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=vk.com
 // @grant        GM.getValue
